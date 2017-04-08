bcce2afbd6b280c7d26b5ada6e9761581a69beb9
shaohaiting <shao113112@gmail.com>
2017-04-08 15:44:57 +0800

    update

--- src/main/java/core/init.java
+++ src/main/java/core/init.java
@@ -1,4 +1,4 @@
-package core;
+package main.java.core;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVOSCloud;
@@ -7,7 +7,7 @@ import com.avos.avoscloud.AVObject;
 public class init {
 
 	public static void main(String[] args){
-	    // 参数依次为 AppId、AppKey、MasterKey
+	    // 鍙傛暟渚濇涓� AppId銆丄ppKey銆丮asterKey
 	    AVOSCloud.initialize("MpolwUmhscbRl3reTw6wfUuo-gzGzoHsz","UUNctBoWK9rt29NYJ0PnB9qV","2VOVpbIK7R2J1vXupD53gaqQ");
 	
 	    AVObject testObject = new AVObject("TestObject");
